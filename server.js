9a1b980218318bdbf4b0f8b92599773bced5675a
unknown <bradleyaw@gmail.com>
2018-03-30 00:30:03 -0400

    Updated logic

--- server.js
+++ server.js
@@ -52,31 +52,27 @@ function productInit() {
             }
         ]).then(function (inqResp) {
             // set several variables to hold response data
-            var itemPurchased = res[inqResp.idSelect].item
-            var userQuantity = inqResp.quantitySelect
-            var dbQuantity = res[inqResp.idSelect].quantity
-            var cost = res[inqResp.idSelect].price * userQuantity;
+            var itemNumber = inqResp.idSelect - 1;
+            var itemPurchased = res[itemNumber].item;
+            var userQuantity = inqResp.quantitySelect;
+            var dbQuantity = res[itemNumber].quantity;
+            var cost = res[itemNumber].price * userQuantity;
+
             // if we have enough stock...
             if (userQuantity <= dbQuantity) {
                 //update our database with new numbers
-                connection.query("UPDATE products SET ? WHERE ?", [{ quantity: dbQuantity - userQuantity}, { item: itemPurchased }], function (err, res) {
+                connection.query("UPDATE products SET ? WHERE ?", [{ quantity: dbQuantity - userQuantity }, { item: itemPurchased }], function (err, res) {
                     if (err) throw err;
-                    console.log(res.affectedRows + " quantities updated!\n");
-                    console.log("-------------------------------------");
                 });
                 console.log("Thank you for your money!");
                 console.log("Your purchase of " + userQuantity + " " + itemPurchased + "(s) has been made for a total of $" + cost + ".");
                 productInit();
-            // if the order is too big...
-            } else if (userQuantity > dbQuantity) {
+                
+            }// if the order is too big... 
+            else if (userQuantity > dbQuantity) {
                 console.log("insufficient quantity");
                 productInit();
-            } else {
-                console.log("Not sure how you messed this up...");
             }
-            
-    
-    
         });
     })
 }
